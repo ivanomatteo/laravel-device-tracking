e65a0cf125b32d7e50b37e288d0cee2016810106
António Gonçalves <94224806+acgtwentyone@users.noreply.github.com>
2022-04-22 11:55:32 -0100

    Update DeviceHijacked.php
    Remove unnecessary imports for performance improvements.

--- src/Events/DeviceHijacked.php
+++ src/Events/DeviceHijacked.php
@@ -2,11 +2,7 @@
 
 namespace IvanoMatteo\LaravelDeviceTracking\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
